2eb9e8a3e315d32416483c5ad21342e16a9f86f2
dangomezcar <dgomezpreco@gmail.com>
2022-02-19 20:01:56 -0500

    Desarrollo del desafío complementario 1

---
+++ js/miarchivo.js
@@ -0,0 +1,39 @@
+console.log('desafio complementario 1');
+
+// Este bloque de código es para for
+
+//let number = prompt ('Dame un número para calcular la sumatoria');
+// let resultado = 0
+
+// for (let i = 0; i<= number; i++ ){
+//     resultado = resultado + i 
+// }
+
+// alert ('La sumatoria de ' + number + ' es ' + resultado);
+
+
+
+// Este bloque de código es para while
+
+// let palabra = ''
+
+// while (palabra != 'margarita'){
+//     palabra = prompt ('Ingrese el nombre de una flor');
+//     if (palabra != 'margarita'){
+//         alert ('Prueba con otra');
+//     }
+//     else if (palabra == 'margarita'){
+//         alert ('Felicitaciones');
+//     }
+// }
+
+
+// Este bloque de código es para do while
+
+// let number = ''
+
+// do {
+//     number = prompt ('Dame un número par');
+// } while (number % 2 == 0)
+
+// alert ('Número impar')
